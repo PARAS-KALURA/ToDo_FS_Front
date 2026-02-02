da44985ed15b555248065be9f312579501a4dece
PARAS-KALURA <112518636+PARAS-KALURA@users.noreply.github.com>
2026-02-02 22:44:42 +0530

    Going

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import axios from 'axios';
 import React, { useEffect, useState } from 'react'
 
 const App = () => {
@@ -8,69 +7,35 @@ const App = () => {
   const [editTodo, setEditTodo] = useState(null);
   const [editedText, setEditedText] = useState("");
   
-  const getTodos = async() => {
-    try {
-      const res = await axios.get("http://localhost:3000/todos");
-      setTodos(res.data); // Take only the todos sent by backend and store them in React state
-      console.log(res.data);
-      
-    } catch(err) {
-      console.error(err.message);
-    }
-  }
-
-  useEffect(() => {
-    getTodos();
-  }, [] );
-
-
-  const onSubmitForm = async (e) => {
-    e.preventDefault(); //“NO page refresh. Stay inside React.”
-
-    try {
-      await axios.post("http://localhost:3000/todos", {
-        description,
-        completed: false,
-      });
-      setDescription("");
-    } catch (err) {
-      console.error(err.message);
-    }
-  }
 
   return (
-    <div className='min-h-screen bg-gray-800 flex justify-center items-center ' >
-      <div className='bg-gray-50 rounded-2xl shadow-xl w-full max-w-lg p-8' >
-        <h1 className='text-4xl font-bold text-gray-800 mb-8 text-center'>PERN TODO APP</h1>
-        <form
-        onSubmit={onSubmitForm}
-        className='flex items-center gap-2 shadow-sm p-2' >
-          <input
-            className=' flex-1 outline-none  rounded px-3 py-2 text-gray-700 placeholder-gray-400 '
-            type="text" value={description} onChange={(e) => { setDescription(e.target.value) }} placeholder='What needs to be done?' required />
 
-          <button className='bg-blue-500 hover:bg-blue-700 px-2 py-1 rounded text-white cursor-pointer' >Add Task</button>
+<div className='min-h-screen flex items-center justify-center bg-gray-800 text-white'>
+   <div className='' >
+     <h1 className='text-2xl font-bold text-center '>PERN TODO APP</h1>
+      
+      <form className="bg-white p-6 rounded shadow-lg flex gap-2">
 
-        </form>
-        <div>
-  {todos.length === 0 ? (
-    <p className="text-gray-600">
-      No tasks available. Add a new task!
-    </p>
-  ) : (
-    <div>
-      {todos.map((todo) => (
-        <div key={todo.id}>
-          <span>{todo.title}</span>
-        </div>
-      ))}
-    </div>
-  )}
+      <input
+  type="text"
+  placeholder="What needs to be done?"
+  value={description}
+  onChange={(e) => setDescription(e.target.value)} required
+  className="px-4 py-2 rounded text-black w-64 outline-none"
+/>
+
+<button
+  type="submit"
+  className="bg-blue-500 px-4 py-2 rounded font-semibold hover:bg-blue-600 cursor-pointer "
+>
+  Add
+</button>
+
+      </form>
+
+   </div>
 </div>
-
-      </div>
-    </div>
-  )
+)
 }
 
-export default App
+export default App  
